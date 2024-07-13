0175c15d11a5c51bf0f04d23c8e22e5628d3327e
tonim <lucasrocha1313@gmail.com>
2024-07-13 11:05:25 -0300

    Configure Carter and MediatR

--- be/Services/Catalog/Catalog.API/Program.cs
+++ be/Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,18 @@
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly);
+});
+
 
 var app = builder.Build();
 
 //Configure the http request pipeline
+app.MapCarter();
 
 app.Run();

46b45d64108dda74e507a0ca572ec4faec90f79a
tonim <lucasrocha1313@gmail.com>
2024-07-23 20:06:27 -0300

    Finish create product logic

--- be/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
+++ be/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -1,5 +1,6 @@
 ﻿using BuildingBlocks.CQRS;
 using Catalog.API.Models;
+using Marten;
 
 namespace Catalog.API.Products.CreateProduct;
 
@@ -12,7 +13,7 @@ public record CreateProductCommand(
 
 public record CreateProductResult(Guid Id);
 
-internal class CreateProductCommandHandler: ICommandHandler<CreateProductCommand, CreateProductResult>
+internal class CreateProductCommandHandler(IDocumentSession session): ICommandHandler<CreateProductCommand, CreateProductResult>
 {
     public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
     {
@@ -25,8 +26,9 @@ internal class CreateProductCommandHandler: ICommandHandler<CreateProductCommand
             Price = command.Price
         };
         
-        //TODO - save to database
+        session.Store(product);
+        await session.SaveChangesAsync(cancellationToken);
 
-        return new CreateProductResult(Guid.NewGuid());
+        return new CreateProductResult(product.Id);
     }
 }

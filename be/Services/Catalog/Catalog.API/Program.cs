a7c1b05932023292981978cb1d496611b399df3c
tonim <lucasrocha1313@gmail.com>
2024-08-05 16:48:35 -0300

    Global exception handler

--- be/Services/Catalog/Catalog.API/Program.cs
+++ be/Services/Catalog/Catalog.API/Program.cs
@@ -1,5 +1,7 @@
 using Carter;
 using Catalog.API.Configuration;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,5 +14,32 @@ var app = builder.Build();
 
 //Configure the http request pipeline
 app.MapCarter();
+
+app.UseExceptionHandler(exceptionHandlerApp =>
+{
+    exceptionHandlerApp.Run(async context=>
+    {
+        var exception = context.Features.Get<IExceptionHandlerFeature>()?.Error;
+        if(exception == null)
+        {
+            return;
+        }
+
+        var problemDetails = new ProblemDetails
+        {
+            Title = exception.Message,
+            Status = StatusCodes.Status500InternalServerError,
+            Detail = exception.StackTrace
+        };
+
+        var logger = context.RequestServices.GetRequiredService<ILogger<Program>>();
+        logger.LogError(exception, exception.Message);
+        
+        context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+        context.Response.ContentType = "application/problem+json";
+        
+        await context.Response.WriteAsJsonAsync(problemDetails);
+    });
+});
 
 app.Run();

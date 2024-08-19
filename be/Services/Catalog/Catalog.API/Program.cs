afd9d7bad805585d7ab7afb02e310bc63b154ab8
tonim <lucasrocha1313@gmail.com>
2024-08-19 15:33:48 -0300

    Add health check to catalog API

--- be/Services/Catalog/Catalog.API/Program.cs
+++ be/Services/Catalog/Catalog.API/Program.cs
@@ -1,16 +1,27 @@
 using BuildingBlocks.Exceptions.Handler;
 using Carter;
 using Catalog.API.Configuration;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddServices(builder.Configuration, builder.Environment.IsDevelopment());
+
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
+
+builder.Services.AddHealthChecks()
+    .AddNpgSql(builder.Configuration.GetConnectionString("Database")!);
 
 var app = builder.Build();
 
 //Configure the http request pipeline
 app.MapCarter();
 app.UseExceptionHandler(o => {});
+
+app.UseHealthChecks("/health", new HealthCheckOptions
+{
+    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+});
 
 app.Run();

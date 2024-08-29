2d796af451057f2fda8aa76ef200eff296300864
tonim <lucasrocha1313@gmail.com>
2024-08-29 20:06:09 -0300

    register MediatR and Carter

--- be/Services/Basket/Basket.API/Program.cs
+++ be/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,20 @@
+using BuildingBlocks.Behaviors;
+using Carter;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var assembly = typeof(Program).Assembly;
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssemblies(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
 
 var app = builder.Build();
 
@@ -13,5 +24,7 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.MapCarter();
 
 app.Run();

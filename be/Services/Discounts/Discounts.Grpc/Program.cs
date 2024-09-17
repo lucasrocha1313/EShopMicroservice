2cbc7048f328af03d9d5cd8b4b8404500f7843f1
tonim <lucasrocha1313@gmail.com>
2024-09-17 20:37:22 -0300

    Start Discounts Service

---
+++ be/Services/Discounts/Discounts.Grpc/Program.cs
@@ -0,0 +1,35 @@
+using Discounts.Grpc.Services;
+using Microsoft.OpenApi.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+//TODO: Remove swagger if it do not work
+builder.Services.AddGrpc().AddJsonTranscoding();
+builder.Services.AddGrpcSwagger();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1",
+        new OpenApiInfo { Title = "Discount gRPC transcoding", Version = "v1" });
+    
+    var filePath = Path.Combine(AppContext.BaseDirectory, "Discounts.Grpc.xml");
+    c.IncludeXmlComments(filePath);
+    c.IncludeGrpcXmlComments(filePath, includeControllerXmlComments: true);
+});
+
+var app = builder.Build();
+
+//TODO: Remove swagger if it do not work
+app.UseSwagger();
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI();
+}
+
+// Configure the HTTP request pipeline.
+app.MapGrpcService<GreeterService>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+
+app.Run();

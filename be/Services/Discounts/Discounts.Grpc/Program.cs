db0f5a0c551a6b992ee0ac09fc93009738d71176
tonim <lucasrocha1313@gmail.com>
2024-09-26 16:31:58 -0300

    start implementing DiscountService

--- be/Services/Discounts/Discounts.Grpc/Program.cs
+++ be/Services/Discounts/Discounts.Grpc/Program.cs
@@ -27,7 +27,7 @@ if(app.Environment.IsDevelopment())
 }
 
 // Configure the HTTP request pipeline.
-app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DicountService>();
 app.MapGet("/",
     () =>
         "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");

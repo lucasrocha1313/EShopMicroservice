3834eedc2a06cc6c2e9daa442518a3adc8fa915b
tonim <lucasrocha1313@gmail.com>
2024-10-05 16:05:36 -0300

    Connect basket service to discount service

--- be/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
+++ be/Services/Basket/Basket.API/Basket/StoreBasket/StoreBasketHandler.cs
@@ -1,6 +1,7 @@
 using Basket.API.Data;
 using Basket.API.Models;
 using BuildingBlocks.CQRS;
+using Discount.Grpc;
 
 namespace Basket.API.Basket.StoreBasket;
 
@@ -9,13 +10,25 @@ public record StoreBasketCommand(ShoppingCart Cart): ICommand<StoreBasketResult>
 
 
 
-public class StoreBasketCommandHandler(IBasketRepository repository): ICommandHandler<StoreBasketCommand, StoreBasketResult>
+public class StoreBasketCommandHandler(IBasketRepository repository, DiscountService.DiscountServiceClient discountService)
+    : ICommandHandler<StoreBasketCommand, StoreBasketResult>
 {
     public async Task<StoreBasketResult> Handle(StoreBasketCommand request, CancellationToken cancellationToken)
     {
         var cart = request.Cart;
+        await DeductDiscount(cart, cancellationToken);
         
         await repository.StoreBasket(cart, cancellationToken);
         return new StoreBasketResult(cart.UserName);
+    }
+
+    private async Task DeductDiscount(ShoppingCart cart, CancellationToken cancellationToken)
+    {
+        foreach (var item in cart.Items)
+        {
+            var coupon = await discountService.GetDiscountAsync(
+                new GetDiscountRequest { ProductName = item.ProductName }, cancellationToken: cancellationToken);
+            item.Price -= coupon.Amount;
+        }
     }
 }

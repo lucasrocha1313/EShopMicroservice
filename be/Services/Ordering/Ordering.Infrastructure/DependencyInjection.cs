af81c72906867f3b0acea8bd95b3ad3609d3314d
tonim <lucasrocha1313@gmail.com>
2024-11-02 11:28:45 -0300

    Configure SQL server on DependencyInjection

--- be/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ be/Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,7 @@
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Infrastructure.Data;
 
 namespace Ordering.Infrastructure;
 
@@ -7,7 +9,13 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
     {
-        // var connectionString = configuration.GetConnectionString("Database");
+        var connectionString = configuration.GetConnectionString("Database");
+        services.AddDbContext<ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+
+        // services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
         
         return services;
     }

681bb2ca2ab453fc2c49416d57a94deaba2b67f4
tonim <lucasrocha1313@gmail.com>
2025-02-02 11:12:13 -0300

    Load products

--- fe/e-shop-web/src/app/[locale]/page.tsx
+++ fe/e-shop-web/src/app/[locale]/page.tsx
@@ -1,96 +1,61 @@
-import Image from "next/image";
+"use client";
+
 import styles from "./page.module.css";
+import { useState, useEffect } from "react";
+
+
+const API_URL = "http://localhost:6004/catalog-service/products"; //TODO - move to env
+
+async function getProducts(): Promise<Product[]> {
+  const res = await fetch(API_URL, { cache: "no-store" }); // no-store ensures fresh data
+  const data: ProductResponse =  await res.json();
+  return data.products;
+}
+
 
 export default function Home() {
+
+  const [products, setProducts] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  //TODO move this to a component
+  //TODO create product card
+  //TODO create Product grid
+  useEffect(() => {
+    getProducts()
+      .then((data) => {
+        setProducts(data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error("Failed to load products:", error);
+        setLoading(false); 
+      });
+  }, []);
+
+
+  
+
+  if (loading) {
+    return <div>Loading products...</div>;
+  }
+
+  if (products.length === 0) {
+    return <div>No products found.</div>;
+  }
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+          <h1>Products</h1>
+          <ul>
+            {products.map((product) => (
+              <li key={product.id}>{product.name} - ${product.price}</li>
+            ))}
+          </ul>
+        </div>  
+      </main>      
     </div>
   );
 }
